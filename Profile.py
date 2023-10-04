2f0880fe1108c1f9b5e988f6b97ddc771cfede9f
Romit-Kumar18 <113458536+Romit-Kumar18@users.noreply.github.com>
2023-10-04 09:29:29 +0530

    consolidated verification functions

--- Profile.py
+++ Profile.py
@@ -1,28 +1,35 @@
 import json
 import base64
-import PPassHash as ph
+import bcrypt
 
-def saveprofile(userid,passwd):    
+def encrypt(passwd):
+    bpasswd = passwd.encode('utf-8')
+    salt = bcrypt.gensalt(rounds=10)
+    hashed_passwd = bcrypt.hashpw(bpasswd, salt)
+    return hashed_passwd
+
+def save_profile(userid,passwd): 
     with open('Profiles.json','r+') as dbfile:
         try:
             profiles=json.load(dbfile)
         except json.JSONDecodeError:
             profiles={}
-        profiles[userid]=base64.b64encode(ph.encrypt(passwd)).decode('utf-8')
+        profiles[userid]=base64.b64encode(encrypt(passwd)).decode('utf-8')
         dbfile.seek(0)
         json.dump(profiles, dbfile, indent=2)
         dbfile.truncate()
 
-def verifypass(userid,passwd):
+def verify_profile(userid,passwd):
     verification=False
     with open('Profiles.json','r') as dbfile:
-        profiles=json.load(dbfile)
         try:
-            hashed_passwd=profiles[userid]
-            hashed_passwd = base64.b64decode(hashed_passwd.encode('utf-8'))
-            verification=ph.verify(passwd,hashed_passwd)
-        except KeyError:
-            print("User does not exist")
+            profiles=json.load(dbfile)
+            hashed_passwd_str=profiles[userid]
+            hashed_passwd=base64.b64decode(hashed_passwd_str.encode('utf-8'))
+            bpasswd=passwd.encode('utf-8')
+            if bcrypt.checkpw(bpasswd, hashed_passwd):
+                verification=True
+        except (KeyError,json.JSONDecodeError):
+            print("User does not exist or JSON decode error")
     dbfile.close()
     return verification
-verifypass("Name2","p")
