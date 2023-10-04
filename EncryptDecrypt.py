5838bff266e38325c1b828a7e75b8ca6bb17714a
Romit-Kumar18 <113458536+Romit-Kumar18@users.noreply.github.com>
2023-10-05 01:38:14 +0530

    main

---
+++ EncryptDecrypt.py
@@ -0,0 +1,31 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import os
+
+def generate_key(user_passwd,salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        iterations=100000,
+        salt=salt,
+        length=32,
+        backend=default_backend()
+    )
+    key = kdf.derive(user_passwd.encode('utf-8'))
+    return key
+
+def encrypt(stored_passwd,key):
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(stored_passwd.encode('utf-8')) + encryptor.finalize()
+    return iv + ciphertext
+
+def decrypt(ciphertext,key):
+    iv = ciphertext[:16]
+    ciphertext = ciphertext[16:]
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+    return plaintext.decode('utf-8')
--- Profile.py
+++ Profile.py
@@ -1,20 +1,24 @@
 import json
 import base64
 import bcrypt
+import EncryptDecrypt
 
-def encrypt(passwd):
+def hashing(passwd):
     bpasswd = passwd.encode('utf-8')
     salt = bcrypt.gensalt(rounds=10)
     hashed_passwd = bcrypt.hashpw(bpasswd, salt)
-    return hashed_passwd
+    return hashed_passwd,salt
 
-def save_profile(userid,passwd): 
+def save_profile(userid,passwd):
     with open('Profiles.json','r+') as dbfile:
+        hashed_passwd,salt=hashing(passwd)
+        key=EncryptDecrypt.generate_key(passwd,salt)
         try:
             profiles=json.load(dbfile)
         except json.JSONDecodeError:
             profiles={}
-        profiles[userid]=base64.b64encode(encrypt(passwd)).decode('utf-8')
+        passwd_and_key=[base64.b64encode(hashed_passwd).decode('utf-8'),base64.b64encode(key).decode('utf-8')]
+        profiles[userid]=passwd_and_key
         dbfile.seek(0)
         json.dump(profiles, dbfile, indent=2)
         dbfile.truncate()
@@ -24,12 +28,14 @@ def verify_profile(userid,passwd):
     with open('Profiles.json','r') as dbfile:
         try:
             profiles=json.load(dbfile)
-            hashed_passwd_str=profiles[userid]
-            hashed_passwd=base64.b64decode(hashed_passwd_str.encode('utf-8'))
+            hashed_passwd_str=profiles[userid[0]]
+            hashed_passwd=base64.b64decode(hashed_passwd_str[0].encode('utf-8'))
             bpasswd=passwd.encode('utf-8')
             if bcrypt.checkpw(bpasswd, hashed_passwd):
                 verification=True
+                dbfile.close()
+            return verification
         except (KeyError,json.JSONDecodeError):
             print("User does not exist or JSON decode error")
-    dbfile.close()
-    return verification
+            dbfile.close()
+            return verification
